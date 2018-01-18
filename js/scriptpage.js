e5e3f28274dd8f3157e5d5d2aee69070e150bb9b
Vithal R Bonker <vbonker@asiapac.globalcsc.net>
2018-01-18 22:53:01 +0530

    implemented tree data poulating with AJAX GET & POST methods

--- js/scriptpage.js
+++ js/scriptpage.js
@@ -1,9 +1,16 @@
 const common = require('./common');
+var fs = require('fs');
 
 module.exports = app => {
     app.get('/scriptpage', function(request, response){
        //common.writeConsoleMessage("*******************SCRIPT PAGE RENDER START****************");
        response.render('ScriptPage.ejs');
        //common.writeConsoleMessage("*******************SCRIPT PAGE RENDER END****************");
+    });
+
+    app.post('/api/tree', function(request, response, next){      
+      fs.writeFile('data/tree.json', JSON.stringify(request.body), function(err){
+         if(err) throw err;
+      });
     });
 };
