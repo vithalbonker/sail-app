6654b8e1e0df1a95c9e082ec2424ebee91c69983
Vithal R Bonker <vbonker@asiapac.globalcsc.net>
2018-01-19 16:36:56 +0530

    tree changes

--- js/scriptpage.js
+++ js/scriptpage.js
@@ -8,9 +8,20 @@ module.exports = app => {
        //common.writeConsoleMessage("*******************SCRIPT PAGE RENDER END****************");
     });
 
-    app.post('/api/tree', function(request, response, next){      
+    app.get('/api/tree', function(request, response){
+        fs.readFile('data/tree.json', 'utf8', function(err, data) {
+           if (err) throw err;
+           var jsonObj = JSON.parse(data);
+           response.json(jsonObj);
+        });        
+    });
+
+    app.post('/api/tree', function(request, response, next){
+      //console.log(JSON.stringify(request.body));
       fs.writeFile('data/tree.json', JSON.stringify(request.body), function(err){
          if(err) throw err;
+         response.sendStatus(200);
+         next();
       });
     });
 };
