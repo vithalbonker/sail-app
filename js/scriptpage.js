f56b15e388a14b458866dd3f972cf5bb7ff7ea13
Vithal R Bonker <vbonker@asiapac.globalcsc.net>
2018-01-19 22:42:32 +0530

    cleaning code & added comments for reference purpose

--- js/scriptpage.js
+++ js/scriptpage.js
@@ -14,21 +14,12 @@ module.exports = app => {
     app.get('/api/tree', function(request, response){
         fs.readFile('data/tree.json', 'utf8', function(err, data) {
            if (err) throw err;
-           //var jsonObj = JSON.parse(data);
-           console.log(data);
            response.send(JSON.parse(data));
         });
     });
 
     app.post('/api/tree', function(request, response, next){
-      var treeData = JSON.stringify(request.body)
-
-      // fs.writeFile('data/tree.json', treeData, function(err){
-      //    if(err) throw err;
-      //    response.sendStatus(200);
-      //    next();
-      // });
-
+      var treeData = JSON.stringify(request.body)      
       child = exec("java utils.WriteTreeDataToJson " + treeData , function (error, stdout, stderr) {
           if (error !== null) {
             common.writeConsoleMessage('exec error: ' + error);
